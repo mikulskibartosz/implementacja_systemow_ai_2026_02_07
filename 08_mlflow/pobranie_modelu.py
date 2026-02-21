7daaeafaa5d6c8eeafb9eb5d7108574ca421a216
Bartosz Mikulski <mail@mikulskibartosz.name>
2026-02-21 14:44:37 +0100

    model registry

--- 08_mlflow/pobranie_modelu.py
+++ 08_mlflow/pobranie_modelu.py
@@ -5,7 +5,19 @@ iris_features = [5.1, 3.5, 1.4, 0.2]
 run_id = "90bcd1086f384bf4b1d0a33479d1fac0"
 model_uri = f"runs:/{run_id}/model"
 
+print("Pobranie z Run")
+
 loaded_model = mlflow.sklearn.load_model(model_uri)
+print(loaded_model)
+
+prediction = loaded_model.predict([iris_features])
+print(prediction)
+
+print("Pobranie z ModelRegistry")
+model_name = "IrisClassifier"
+model_version = 1
+
+loaded_model = mlflow.sklearn.load_model(f"models:/{model_name}/{model_version}")
 print(loaded_model)
 
 prediction = loaded_model.predict([iris_features])
